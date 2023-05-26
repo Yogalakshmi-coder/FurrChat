03d3186ab7b7574002f4131b5d87f4d7e21f170a
Yogalakshmi <byogalakshmi88@gmail.com>
2023-05-26 14:26:50 -0700

    Firestore connected for read and write

--- FurrChat/Controllers/ChatViewController.swift
+++ FurrChat/Controllers/ChatViewController.swift
@@ -7,18 +7,69 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ChatViewController: UIViewController {
     
-    @IBOutlet weak var MessageTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    var messages: [Message] = []
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableview.dataSource = self
+        
+        tableview.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
+    }
+    
+    func loadMessages() {
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            self.messages = []
+            if let e = error {
+                print("Error in reading data from firestore, \(String(describing: e.localizedDescription))")
+            
+            } else {
+                if let SnapshotDoc = querySnapshot?.documents {
+                    for docs in SnapshotDoc {
+                        let data = docs.data()
+                        if let messageSender = data[K.FStore.senderField] as? String,
+                           let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            print(newMessage)
+                            self.messages.append(newMessage)
+                            DispatchQueue.main.async {
+                                self.tableview.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
     }
    
     @IBAction func sendKeyPressed(_ sender: UIButton) {
-        
+        if let messageBody = messageTextField.text,
+           let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
+            ]) { error in
+                if let e = error {
+                    print("Error saving the data in firestore, \(e.localizedDescription)")
+                } else {
+                    print("Data saved successfully")
+                }
+            }
+        }
     }
     
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
@@ -29,5 +80,19 @@ class ChatViewController: UIViewController {
             print("Error in signing out, \(signOutError)")
         }
     }
+}
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(messages.count)
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+    
+    
 }
 
---
+++ FurrChat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FurrChat
+//
+//  Created by Yogalakshmi Balasubramaniam on 5/26/23.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
